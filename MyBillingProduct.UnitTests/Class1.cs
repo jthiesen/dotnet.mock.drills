a160f8b77689cd51531f6748ccce33366a376639
Jesper Laursen Thiesen <jesper.l.thiesen@simcorp.com>
2020-08-25 11:22:26 +0200

    Session 2

--- MyBillingProduct.UnitTests/Class1.cs
+++ MyBillingProduct.UnitTests/Class1.cs
@@ -1,17 +1,19 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 using NUnit.Framework;
 
 namespace MyBillingProduct.UnitTests
 {
     //rename this class as needed
-    public class Class1
+    public class LoginManager1Tests
     {
         [TestCase("a", "pass")]
         [TestCase("b", "pass")]
         public void IsLoginOK_IfCredentialsCorrect_ShouldLogOK(string user, string pass)
         {
             var log = new FakeLogger();
-            LoginManager1 lm = new LoginManager1(log);
+            var webservice = new FakeWebService();
+            LoginManager1 lm = new LoginManager1(log, webservice);
             lm.AddUser(user, pass);
             lm.IsLoginOK(user, pass);
             Assert.That(log.GetLastLog().Equals($"login ok: user: [{user}]"));
@@ -22,23 +24,65 @@ namespace MyBillingProduct.UnitTests
         public void IsLoginOK_IfUnknwonUser_ShouldLogBadLogin(string user, string pass)
         {
             var log = new FakeLogger();
-            LoginManager1 lm = new LoginManager1(log);
+            var webservice = new FakeWebService();
+            LoginManager1 lm = new LoginManager1(log, webservice);
             lm.AddUser("a", pass);
             lm.IsLoginOK(user, pass);
             Assert.AreEqual(log.GetLastLog(), $"bad login: [{user}],[{pass}]");
         }
 
-        [Test]
-        public void IsLoginOK_IfLoggerThrowsException_NotifyWebService() 
+        [TestCase("a", "pass")]
+        public void IsLoginOK_IfLoggerThrowsException_NotifyWebService(string user, string pass) 
         {
-    
-                
+            var log = new FakeLogger();
+            log.simulateException = true;
+            var webservice = new FakeWebService();
+            LoginManager1 lm = new LoginManager1(log, webservice);
+            lm.AddUser(user, pass);
+            lm.IsLoginOK(user, pass);
+            StringAssert.Contains(webservice.GetLastLog(), $"got exception - [message]");
+        }
+
+        [TestCase("a", "pass")]
+        public void AddUser_IfLoggerThrowsException_NotifyWebService(string user, string pass) 
+        {
+            var log = new FakeLogger();
+            log.simulateException = true;
+            var webservice = new FakeWebService();
+            LoginManager1 lm = new LoginManager1(log, webservice);
+            lm.AddUser(user, pass);
+            StringAssert.Contains($"got exception - [message]",webservice.GetLastLog());
+        }
+
+        [TestCase("a", "password")]
+        [TestCase("b", "password")]
+        public void AddUser_WhenCalled_ShouldLog(string user, string pass)
+        {
+            var log = new FakeLogger();
+            var webservice = new FakeWebService();
+            LoginManager1 lm = new LoginManager1(log, webservice);
+            lm.AddUser(user, pass);
+            Assert.That(log.GetLastLog().Equals($"user added: [{user}],[{pass}]"));
+        }
+
+    }
+
+    public class FakeWebService : IWebService {
+        private string loggedMessage = "";
+        public void Write(string message) {
+            loggedMessage = message;
+        }
+        
+        public string GetLastLog()
+        {
+            return loggedMessage;
         }
     }
 
     public class FakeLogger : ILogger
     {
         private string loggedMessage = "";
+        public bool simulateException = false;
 
         public string GetLastLog()
         {
@@ -47,6 +91,9 @@ namespace MyBillingProduct.UnitTests
 
         public void Write(string text)
         {
+            if (simulateException) {
+                throw new LoggerException();
+            }
             loggedMessage = text;
         }
     }
--- MyBillingProduct/LoginManager1.cs
+++ MyBillingProduct/LoginManager1.cs
@@ -7,28 +7,34 @@ namespace MyBillingProduct
 	{
 	    private Hashtable m_users = new Hashtable();
         private ILogger m_log;
+        private readonly IWebService m_webService;
 
-        public LoginManager1(ILogger log)
-        {
-            m_log = log;
+        public LoginManager1(ILogger log, IWebService fakeWebService) {
+	        m_log = log;
+	        m_webService = fakeWebService;
         }
 
         public bool IsLoginOK(string user, string password)
 	    {
-            if (m_users[user] != null &&
-                (string) m_users[user] == password)
-	        {
-                m_log.Write($"login ok: user: [{user}]");
-				return true;
+		    try {
+			    if (m_users[user] != null &&
+			        (string) m_users[user] == password) {
+				    m_log.Write($"login ok: user: [{user}]");
+				    return true;
 
-	        }
-            m_log.Write($"bad login: [{user}],[{password}]");
-			return false;
+			    }
+			    m_log.Write($"bad login: [{user}],[{password}]");
+			    return false;
+		    }catch (LoggerException) {
+			    m_webService.Write("got exception - [message]");
+			    return false;
+		    }
 	    }
 
 	    public void AddUser(string user, string password)
 	    {
 	        m_users[user] = password;
+	        // m_log.Write($"user added: [{user}],[{password}]");
 	    }
 
 	    public void ChangePass(string user, string oldPass, string newPassword)
