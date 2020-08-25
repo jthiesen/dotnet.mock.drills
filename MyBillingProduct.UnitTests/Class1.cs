e0b270ddc4cd9c55877a75abf47b9b1bffc82245
Jesper Laursen Thiesen <jesper.l.thiesen@simcorp.com>
2020-08-25 10:13:58 +0200

    Session 1 complete

--- MyBillingProduct.UnitTests/Class1.cs
+++ MyBillingProduct.UnitTests/Class1.cs
@@ -1,16 +1,53 @@
-﻿using NUnit.Framework;
+﻿using System.Runtime.InteropServices;
+using NUnit.Framework;
 
 namespace MyBillingProduct.UnitTests
 {
     //rename this class as needed
     public class Class1
     {
-        [Test]
-        public void FirstTest()
+        [TestCase("a", "pass")]
+        [TestCase("b", "pass")]
+        public void IsLoginOK_IfCredentialsCorrect_ShouldLogOK(string user, string pass)
         {
-            LoginManager1 lm = new LoginManager1();
-            lm.AddUser("a", "pass");
-           //? 
+            var log = new FakeLogger();
+            LoginManager1 lm = new LoginManager1(log);
+            lm.AddUser(user, pass);
+            lm.IsLoginOK(user, pass);
+            Assert.That(log.GetLastLog().Equals($"login ok: user: [{user}]"));
+        }
+
+        [TestCase("b", "pass")]
+        [TestCase("c", "pass")]
+        public void IsLoginOK_IfUnknwonUser_ShouldLogBadLogin(string user, string pass)
+        {
+            var log = new FakeLogger();
+            LoginManager1 lm = new LoginManager1(log);
+            lm.AddUser("a", pass);
+            lm.IsLoginOK(user, pass);
+            Assert.AreEqual(log.GetLastLog(), $"bad login: [{user}],[{pass}]");
+        }
+
+        [Test]
+        public void IsLoginOK_IfLoggerThrowsException_NotifyWebService() 
+        {
+    
+                
+        }
+    }
+
+    public class FakeLogger : ILogger
+    {
+        private string loggedMessage = "";
+
+        public string GetLastLog()
+        {
+            return loggedMessage;
+        }
+
+        public void Write(string text)
+        {
+            loggedMessage = text;
         }
     }
 }
--- MyBillingProduct/LoginManager1.cs
+++ MyBillingProduct/LoginManager1.cs
@@ -6,16 +6,24 @@ namespace MyBillingProduct
 	public class LoginManager1
 	{
 	    private Hashtable m_users = new Hashtable();
+        private ILogger m_log;
 
-        
-	    public bool IsLoginOK(string user, string password)
+        public LoginManager1(ILogger log)
+        {
+            m_log = log;
+        }
+
+        public bool IsLoginOK(string user, string password)
 	    {
             if (m_users[user] != null &&
-	            (string) m_users[user] == password)
+                (string) m_users[user] == password)
 	        {
-	            return true;
+                m_log.Write($"login ok: user: [{user}]");
+				return true;
+
 	        }
-	        return false;
+            m_log.Write($"bad login: [{user}],[{password}]");
+			return false;
 	    }
 
 	    public void AddUser(string user, string password)
